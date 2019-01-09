65155b4ba7b86d7fbd188d44a89b74c254dc3f03
Lindsay Wilson <lindsaywilsonlcw@gmail.com>
2019-01-09 22:12:53 +0100

    rename files, test turnRight function, add moveForward suite

--- test/failing-test-spec.js
+++ test/failing-test-spec.js
@@ -9,3 +9,125 @@ describe('A failing test', function () {
   })
 })
 
+
+// needs to have a position
+// describe('receives the position of the rover in integers x,y and a string as the direction "N, E, S, W"', function() {
+//   var rover;
+
+//   before(function(){
+//     rover = {
+//       x: 0,
+//       y: 0,
+//       direction: 'N',
+//       position: ''
+//     }
+//   })
+
+//   it('should initialise start position', function() {
+//     expect(rover.x).to.be
+//   })
+// })
+
+
+
+describe('turnLeft', function(){
+  var turnLeft = require("../js/index").turnLeft;
+
+  it('should turn the rover from N to W', function() {
+    var rover = {
+      x: 0,
+      y: 0,
+      direction: 'N'
+    }
+  
+    turnLeft(rover)
+    expect(rover.direction).to.equal('W')
+  })
+
+  it('should turn the rover from W to S', function() {
+    var rover = {
+      x: 0,
+      y: 0,
+      direction: 'W'
+    }
+  
+    turnLeft(rover)
+    expect(rover.direction).to.equal('S')
+  })
+
+  it('should turn the rover from S to E', function() {
+    var rover = {
+      x: 0,
+      y: 0,
+      direction: 'S'
+    }
+  
+    turnLeft(rover)
+    expect(rover.direction).to.equal('E')
+  })
+
+  it('should turn the rover from E to N', function() {
+    var rover = {
+      x: 0,
+      y: 0,
+      direction: 'E'
+    }
+  
+    turnLeft(rover)
+    expect(rover.direction).to.equal('N')
+  })
+})
+
+describe('turnRight', function(){
+  var turnRight = require('../js/index').turnRight
+
+  it('should turn rover from N to E', function() {
+    var rover = {
+      x: 0,
+      y: 0,
+      direction: 'N'
+    }
+    turnRight(rover)
+    expect(rover.direction).to.equal('E')
+  })
+
+  it('should turn rover from E to S', function() {
+    var rover = {
+      x: 0,
+      y: 0,
+      direction: 'E'
+    }
+    turnRight(rover)
+    expect(rover.direction).to.equal('S')
+  })
+
+  it('should turn rover from S to W', function() {
+    var rover = {
+      x: 0,
+      y: 0,
+      direction: 'S'
+    }
+    turnRight(rover)
+    expect(rover.direction).to.equal('W')
+  })
+
+  it('should turn rover from W to N', function() {
+    var rover = {
+      x: 0,
+      y: 0,
+      direction: 'W'
+    }
+    turnRight(rover) 
+    expect(rover.direction).to.equal('N') 
+  })
+})
+
+
+describe('moveForward', function() {
+  var moveForward = require('../js/index')
+
+  it('should move rover one space north when facing north')
+  moveForward(rover)
+  expect(rover.y).to.equal(rover.y +1)
+})
+
